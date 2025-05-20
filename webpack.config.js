731277b59c5fc2171d13928469b9768772235f8d
koshkina-ma <79514417725@ya.ru>
2025-05-20 22:48:36 +0500

    1.1 Установит и настроит сборщик (webpack)

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'), //проект лежит в папке с ! в названии, не собираюсь переносить, поэтому путь прописан через 
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],  
+};
+
+
+
+
+
