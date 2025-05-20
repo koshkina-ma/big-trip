84fa0036096e4b930dad9858f51b51f1fed4c0df
koshkina-ma <79514417725@ya.ru>
2025-05-20 23:17:47 +0500

    1.3 Установит и настроит Babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,22 @@ module.exports = {
       patterns: [{ from: 'public' }],
     }),
   ],  
+
+module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
+
 };
 
 
