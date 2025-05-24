9a89cde68dcf5d99c4900a3862a11d54ebbdd56f
koshkina-ma <79514417725@ya.ru>
2025-05-24 22:59:27 +0500

    починила хеадер для первой домашки

--- src/view/list-filter-view.js
+++ src/view/list-filter-view.js
@@ -2,10 +2,7 @@ import {createElement} from '../render.js';
 
 function createListFilterTemplate() {
   return (/*html*/
-    `<div class="trip-main__trip-controls  trip-controls">
-            <div class="trip-controls__filters">
-              <h2 class="visually-hidden">Filter events</h2>
-              <form class="trip-filters" action="#" method="get">
+    `<form class="trip-filters" action="#" method="get">
                 <div class="trip-filters__filter">
                   <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
                   <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
@@ -27,9 +24,7 @@ function createListFilterTemplate() {
                 </div>
 
                 <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>
-            </div>
-          </div>`
+              </form>`
 
   );
 }
